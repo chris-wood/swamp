665dca5cedd54f6a1884c450d9a2daa56c0114a1
Christopher Wood <christopherwood07@gmail.com>
2016-02-11 11:04:34 -0800

    pull request

--- src/ccnkv.py
+++ src/ccnkv.py
@@ -30,8 +30,17 @@ class CCNxClient(object):
         portal = factory.create_portal(transport=TransportType_RTA_Message, attributes=PortalAttributes_NonBlocking)
         return portal
 
-    def get(self, name, data):
+    def get(self, name, data = ""):
         interest = Interest(Name(name), payload=data)
+        self.portal.send(interest)
+        response = self.portal.receive()
+        if isinstance(response, ContentObject):
+            return response.getPayload()
+        else:
+            return None
+
+    def get_by_identifier(self, name, identifier):
+        interest = Interest(Name(name), content_object_hash=identifier)
         self.portal.send(interest)
         response = self.portal.receive()
         if isinstance(response, ContentObject):
@@ -59,13 +68,46 @@ class CCNxClient(object):
                     raise
         return None
 
-    def push(self, name, data):
+    def force_push(self, name, data):
         interest = Interest(Name(name), payload=data)
         try:
             self.portal.send(interest)
         except Portal.CommunicationsError as x:
             sys.stderr.write("ccnxPortal_Write failed: %d\n" % (x.errno,))
         pass
+
+    def push(self, name, data, prefix):
+        # 1. chunk the data
+        chunks = []
+        chunker = Chunker(data)
+        for chunk in chunker:
+            chunks.append(chunks)
+
+        # 2. send a pull request
+        request = PullRequest()
+        request.name = prefix
+        request.chunks = len(chunks)
+        interest = Interest(Name(name), payload=request.SerializeToString())
+        try:
+            self.portal.send(interest)
+        except Portal.CommunicationsError as x:
+            sys.stderr.write("ccnxPortal_Write failed: %d\n" % (x.errno,))
+        pass
+
+        # 3. respond to each chunk
+        sent = []
+        num_chunks = len(chunks)
+        while (len(sent) < num_chunks):
+            name, payload = self.receive_raw()
+            if name.startsWith(prefix):
+                chunk_number = int(name[-1])
+                chunk = chunks[chunk_number]
+                self.reply(name, chunk)
+
+                if chunk_number not in sent:
+                    sent.append(chunk_number)
+            else:
+                pass
 
     def listen(self, prefix):
         try:
